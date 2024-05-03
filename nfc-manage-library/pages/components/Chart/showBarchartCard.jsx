c59d10db34fe02ee352fcb9b859825aeb2f25c7a
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2024-05-03 08:07:52 +0700

    Update Dashboard

---
+++ nfc-manage-library/pages/components/Chart/showBarchartCard.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+// Register the chart.js components we will use
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: 'Game Title',
+        font: {
+          size: 16,
+          weight: 'bold',
+        },
+      },
+    },
+    y: {
+      title: {
+        display: true,
+        text: 'Scan Count',
+        font: {
+          size: 16,
+          weight: 'bold',
+        },
+      },
+    }
+  },
+};
+
+
+
+const BarChartCardPage = ({ data }) => {
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{
+      label: 'Scan Counts',
+      data: [],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    }],
+  });
+
+  useEffect(() => {
+    const labels = data?.map(item => item.title_card);
+    const dataPoints = data?.map(item => item.count_scan_card);
+    console.log(labels,"labels")
+    console.log(dataPoints,"dataPoints")
+    setChartData({
+      labels: labels,
+      datasets: [{
+        label: 'Scan Counts',
+        data: dataPoints,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }]
+    });
+  }, [data]);
+
+
+  return (
+    <div className='w-full h-full'>
+      <label className="text-3xl font-bold ">Today Revenue</label>
+      <br/>
+      <label className="text-2xl font-bold ml-4">$ 1000</label>
+      <Bar data={chartData} options={options} />
+    </div>
+  );
+}
+
+export default BarChartCardPage;
